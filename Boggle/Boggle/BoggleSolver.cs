a88edaf5dbdfe346ebc6c9febe49083c4fb08ac1
toston <turushev.dmitriy@gmail.con>
2016-09-25 22:49:19 +0300

    [Cleanup]

--- Boggle/Boggle/BoggleSolver.cs
+++ Boggle/Boggle/BoggleSolver.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using BogWord = System.Collections.Generic.List<Boggle.Letter>;
 
 namespace Boggle
 {
@@ -23,69 +22,78 @@ namespace Boggle
             {
                 for (var j = 0; j < randomBoard.GetLength(1); j++)
                 {
-                    FindWord(i, j, string.Empty, new BogWord(), tree);
+                    FindWord(i, j, string.Empty, new List<Letter>(), tree);
                 }
             }
 
             return new Result(words);
         }
 
-        public void FindWord(int r, int c, string prefix, List<Letter> word, TrieNode parentNode)
+        public void FindWord(int r, int c, string prefix, List<Letter> path, TrieNode parentNode)
         {
+            // Check indexes in bounds
             if (!board.IsInBoardRange(r, c))
             {
                 return;
             }
-
-            if (word.Any(bogLet => bogLet.Column == c && bogLet.Row == r))
+            // Check same cell using
+            if (path.Any(bogLet => bogLet.Column == c && bogLet.Row == r))
+            {
+                return;
+            }
+            // Check max word length
+            if (prefix.Length == board.GetLength(0) * board.GetLength(1) + 1)
             {
                 return;
             }
 
-            //if (prefix.Length == board.GetLength(0) * board.GetLength(1) + 1)
-            //{
-            //    return;
-            //}
-
-            word.Add(new Letter(r, c, board[r, c]));
-
+            // Check if word-part have next character
             TrieNode node;
-
-            if (parentNode.children.TryGetValue(board[r, c], out node))
+            if (!parentNode.children.TryGetValue(board[r, c], out node))
             {
-                if (board[r, c] == 'q')
-                {
-                    prefix += board[r, c];
+                return;
+            }
 
-                    TrieNode node2;
-                    if (node.children.TryGetValue('u', out node2))
-                    {
-                        node = node2;
-                    }
-                    else
-                    {
-                        return;
-                    }
-                    prefix += 'u';
-                }
-                else
-                {
-                    prefix += board[r, c];
-                }
+            var character = board[r, c];
 
-                if (node.IsWord)
-                {
-                    words.Add(prefix);
-                }
+            path.Add(new Letter(r, c, character));
 
-                for (var i = r - 1; i < r + 2; i++)
+            prefix += character;
+
+            if (character == 'q')
+            {
+                // using QHack function because if we have 'Q' letter in cell it's actually 'QU', so
+                // we have get next node for 'U' letter
+                if (QHack(ref node)) return;
+            }
+
+            if (node.IsWord)
+            {
+                words.Add(prefix);
+            }
+
+            // Checking neighbor cells
+            for (var i = r - 1; i < r + 2; i++)
+            {
+                for (var j = c - 1; j < c + 2; j++)
                 {
-                    for (var j = c - 1; j < c + 2; j++)
-                    {
-                        FindWord(i, j, prefix, word.ToList(), node);
-                    }
+                    FindWord(i, j, prefix, path.ToList(), node);
                 }
             }
+        }
+
+        private static bool QHack(ref TrieNode node)
+        {
+            TrieNode node2;
+            if (node.children.TryGetValue('u', out node2))
+            {
+                node = node2;
+            }
+            else
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
