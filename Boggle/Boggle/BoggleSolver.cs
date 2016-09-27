51c282f5033af359a5c325006b46eba72119d591
torston <turushev.dmitriy@gmail.com>
2016-09-27 12:23:18 +0300

    Added board and dictionary validation

--- Boggle/Boggle/BoggleSolver.cs
+++ Boggle/Boggle/BoggleSolver.cs
@@ -1,12 +1,18 @@
-﻿namespace Boggle
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Boggle
 {
     public class BoggleSolver : ISolver
     {
-        private IWordFinder _wordFinder;
-        private ITrieHelper _trieHelper;
-        private IWordsRepository _wordsRepository;
+        private static readonly Regex EnglishLettersPattern = new Regex("^[a-zA-Z]*$", RegexOptions.Compiled);
 
-        private TrieNode cachedTrie;
+        private readonly IWordFinder _wordFinder;
+        private readonly ITrieHelper _trieHelper;
+        private readonly IWordsRepository _wordsRepository;
+
+        private TrieNode _cachedTrie;
 
         public BoggleSolver(ITrieHelper trieHelper, IWordsRepository wordsRepository, IWordFinder wordFinder)
         {
@@ -17,15 +23,53 @@
 
         public IResults FindWords(char[,] inputBoard)
         {
-            if (cachedTrie == null)
+            ValidateBoard(inputBoard);
+
+            if (_cachedTrie == null)
             {
                 var loadedWords = _wordsRepository.Load();
-                cachedTrie = _trieHelper.MakeTrie(loadedWords);
+                ValidateDictionary(loadedWords);
+
+                _cachedTrie = _trieHelper.MakeTrie(loadedWords);
             }
 
-            var words = _wordFinder.FindWords(inputBoard, cachedTrie);
+            var words = _wordFinder.FindWords(inputBoard, _cachedTrie);
 
             return new Result(words);
+        }
+
+        private static void ValidateBoard(char[,] inputBoard)
+        {
+            if (inputBoard.GetUpperBound(0) < 2 || inputBoard.GetUpperBound(1) < 2)
+            {
+                throw new ArgumentException("Board size should be grater or equals 3x3");
+            }
+
+            foreach (var letter in inputBoard)
+            {
+                if (letter.IsNonEnglishCharacter())
+                {
+                    throw new ArgumentException(
+                        $"Board have non english character: '{letter}', support only english characters");
+                }
+            }
+        }
+
+        private static void ValidateDictionary(IEnumerable<string> loadedWords)
+        {
+            foreach (var loadedWord in loadedWords)
+            {
+                foreach (var letter in loadedWord.ToCharArray())
+                {
+                    if (letter.IsNonEnglishCharacter())
+                    {
+                        throw new ArgumentException(
+                            $"Dictionary word have non english character: '{letter}' in word {loadedWord}," +
+                            " support only english characters");
+                    }
+
+                }
+            }
         }
     }
 }
--- Boggle/Boggle/ExtensionMethods.cs
+++ Boggle/Boggle/ExtensionMethods.cs
@@ -4,11 +4,16 @@
     {
         public static bool IsInBoardRange(this char[,] array, int x, int y)
         {
-            return 
-                x >= array.GetLowerBound(0) && 
-                x <= array.GetUpperBound(0) && 
-                y >= array.GetLowerBound(1) && 
+            return
+                x >= array.GetLowerBound(0) &&
+                x <= array.GetUpperBound(0) &&
+                y >= array.GetLowerBound(1) &&
                 y <= array.GetUpperBound(1);
+        }
+
+        public static bool IsNonEnglishCharacter(this char character)
+        {
+            return character < 63 || character > 126;
         }
     }
 }
